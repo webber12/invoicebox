096e043b653a37ab2a6a87d812ada4fbf2753cb0
webber12 <web-ber12@yandex.ru>
2024-07-03 01:25:22 +0300

    НДС и скидка к корзине

--- assets/plugins/commerce/src/Payments/Invoicebox.php
+++ assets/plugins/commerce/src/Payments/Invoicebox.php
@@ -53,31 +53,42 @@ class Invoicebox extends Payment
 
         $basketItems = [];
         $items = $processor->getCart()->getItems();
+        $discountRate = $this->getDiscountRate($items, $payment);
+        
         foreach($items as $item) {
-            $amount = (float)$item['count'] * (float)$item['price'];
+            $price = (float)$item['price'] * $discountRate; //цена полная с учетом скидки в корзине
+            $amount = (float)$item['count'] * $price; //стоимость полная
+            $price_without_vat = $price / ((100 + (float)$this->getSetting('vat', 0)) / 100); //цена без ндс
+            $price_vat = $price - $price_without_vat; //ндс в цене
+            $amount_without_vat = $price_without_vat * (float)$item['count']; //стоимость без ндс
+            $amount_vat = $price_vat * (float)$item['count']; //общая ндс
+            
             $basketItems[] = new BasketItem(
                 $item['id'],
                 $item['name'],
                 'шт.',
                 '796',
                 $item['count'],
-                $amount,
-                $amount,
-                $amount,
-                0,
-                VatCode::VATNONE,
+                round($price, 2), //Стоимость единицы
+                round($price_without_vat, 2), //Стоимость единицы без учета НДС
+                round($amount), //Стоимость всех единиц с НДС
+                round($amount_vat), //Итого сумма НДС
+                $this->getVatCode(),
                 BasketItemType::COMMODITY,
                 PaymentType::FULL_PREPAYMENT,
                 new \DateTime(date("Y-m-d"))
             );
         }
 
+        $totalAmount = $payment['amount'];
+        $amount_without_vat = $totalAmount / ((100 + (float)$this->getSetting('vat', 0)) / 100); //сумма без ндс
+        
         $request = new CreateOrderRequest(
             $this->lang['invoicebox.order_description'] . ' ' . $order['id'],
             $this->getSetting('merchant_id'), // id магазина
             $order['id'],
-            $payment['amount'],
-            0,
+            $payment['amount'], //Сумма заказа с НДС
+            ($totalAmount - $amount_without_vat), //Сумма НДС
             $currency['code'],
             new \DateTime('tomorrow'),
             $basketItems,
@@ -144,7 +155,43 @@ class Invoicebox extends Payment
         if (!empty($_REQUEST['paymentHash']) && is_scalar($_REQUEST['paymentHash'])) {
             return $_REQUEST['paymentHash'];
         }
-
         return null;
     }
+
+    protected function getVatCode()
+    {
+        $vat = $this->getSetting('vat');
+        switch(true) {
+            case $vat === '':
+                $code = VatCode::VATNONE;
+                break;
+            case $vat == 0:
+                $code = VatCode::RUS_VAT0;
+                break;
+            case $vat == 10:
+                $code = VatCode::RUS_VAT10;
+                break;
+            case $vat == 20:
+                $code = VatCode::RUS_VAT20;
+                break;
+            default:
+                $code = VatCode::VATNONE;
+                break;
+        }
+        return $code;
+    }
+
+    protected function getDiscountRate($items, $payment)
+    {
+        $amount = 0;
+        foreach($items as $item) {
+            $amount += (float)$item['price'] * (float)$item['count'];
+        }
+        if($amount == $payment['amount']) {
+            return 1;
+        } else {
+            return $payment['amount'] / $amount;
+        }
+    }
+    
 }
